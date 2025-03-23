de060db4859b8e80572924db4e56f8a0ea3895cb
ho3eintahan <hoseintahan84@gmail.com>
2025-03-23 10:01:35 +0330

    Update    MainActivity.kt   to handle WebApi

--- app/src/main/java/com/example/apicaller/MainActivity.kt
+++ app/src/main/java/com/example/apicaller/MainActivity.kt
@@ -3,46 +3,48 @@ package com.example.apicaller
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
-import org.json.JSONArray
-import org.json.JSONObject
-import java.io.IOException
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val client: OkHttpClient = OkHttpClient()
+        val videoService = getRetrofit().create(VideoService::class.java)
 
-        val request: Request = Request.Builder()
-            .url("https://jsonplaceholder.typicode.com/todos")
-            .build()
+        val callVideoList = videoService.getVideoList()
 
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                Log.d("tagA", "onFailure: ${e.message}")
-            }
+        callVideoList.enqueue(object:Callback<Movies> {
+            override fun onResponse(call: Call<Movies>, response: Response<Movies>) {
+                
+                val movies=response.body()
 
-            override fun onResponse(call: Call, response: Response) {
-                if (response.isSuccessful) {
-                    val jsonString = response.body!!.string()
-                    val jsonObject = JSONArray(jsonString)
-
-//                    val userId = jsonObject.getInt("userId")
-//                    val id = jsonObject.getInt("id")
-//                    val title = jsonObject.getString("title")
-//                    val completed = jsonObject.getBoolean("completed")
-
-                    Log.d("tagB", "${jsonObject[0]}")
-                } else {
-                    Log.d("tagA", "Error: ${response.code}")
+                if (movies != null) {
+                    Log.d("tagA", "onResponse: ${movies.videoList?.get(0)!!.title}")
                 }
+                
             }
+
+            override fun onFailure(call: Call<Movies>, err: Throwable) {
+                Log.d("tagA", "onResponse: ${err.message}")
+            }
+
         })
+
+
     }
+
+    private fun getRetrofit(): Retrofit {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.aparat.com/etc/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        return retrofit
+    }
+
 }
