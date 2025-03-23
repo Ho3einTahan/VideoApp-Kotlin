90209b3a3799c85890fc14ed3609e7665eb785a6
ho3eintahan <hoseintahan84@gmail.com>
2025-03-23 10:02:15 +0330

    Add    Movies.kt

--- app/src/main/java/com/example/apicaller/Movies.kt
+++ app/src/main/java/com/example/apicaller/Movies.kt
@@ -1,4 +1,10 @@
 package com.example.apicaller
 
+import com.google.gson.annotations.SerializedName
+
 class Movies {
+
+    @SerializedName("mostviewedvideos")
+    var videoList: ArrayList<Video>? = null;
+
 }
