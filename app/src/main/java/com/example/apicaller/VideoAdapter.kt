59e8f613146af0186b5116713c3a1d27da3879b4
ho3eintahan <hoseintahan84@gmail.com>
2025-03-23 12:33:39 +0330

    Add   VideoAdapter.kt

---
+++ app/src/main/java/com/example/apicaller/VideoAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.apicaller
+
+import android.content.Context
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import io.github.farshidroohi.AdapterRecyclerView
+
+class VideoAdapter : AdapterRecyclerView<Video>(R.layout.item_video, 0, 0, 0) {
+    override fun onBindView(
+        viewHolder: RecyclerView.ViewHolder,
+        position: Int,
+        context: Context,
+        element: Video?
+    ) {
+
+        val imageView: ImageView = viewHolder.itemView.findViewById<ImageView>(R.id.imgVideo)
+        val txtTitle: TextView = viewHolder.itemView.findViewById<TextView>(R.id.txtTitle)
+        val title=element!!.title
+        val url: String? = element.smaPoster;
+
+        txtTitle.text= title
+        Glide.with(context).load(url).into(imageView);
+    }
+
+
+}
