730a4bb95d7bb63978523edc68a2ca88bf08ac67
ho3eintahan <hoseintahan84@gmail.com>
2025-03-23 12:35:05 +0330

    Update    MainActivity.kt

--- app/src/main/java/com/example/apicaller/MainActivity.kt
+++ app/src/main/java/com/example/apicaller/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.apicaller
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,23 +17,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview_main)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val videoAdapter = VideoAdapter()
+        recyclerView.adapter = videoAdapter
+
         val videoService = getRetrofit().create(VideoService::class.java)
 
         val callVideoList = videoService.getVideoList()
 
-        callVideoList.enqueue(object:Callback<Movies> {
+        callVideoList.enqueue(object : Callback<Movies> {
             override fun onResponse(call: Call<Movies>, response: Response<Movies>) {
-                
-                val movies=response.body()
+
+                val movies = response.body()
 
                 if (movies != null) {
+                    videoAdapter.loadedState(movies.videoList)
+
                     Log.d("tagA", "onResponse: ${movies.videoList?.get(0)!!.title}")
                 }
-                
+
             }
 
             override fun onFailure(call: Call<Movies>, err: Throwable) {
-                Log.d("tagA", "onResponse: ${err.message}")
+                Log.d("tagA", "onFailure: ${err.message}")
             }
 
         })
